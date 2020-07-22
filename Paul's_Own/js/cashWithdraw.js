6df28217ce058b4e6413d02e04ce84ed8ebad7cf
Paul Davies <68033845+paul0008@users.noreply.github.com>
2020-07-22 23:58:52 +0100

    Delete cashWithdraw.js

--- Paul's_Own/js/cashWithdraw.js
+++
@@ -1,8 +0,0 @@
-const cashWithdrawal = (amount, accnum) => {
-    console.log(`Withdrawing ${amount} from account${accnum}`);
-    
-}
-
-cashWithdrawal(300, 50449921);
-cashWithdrawal(30, 50449921);
-cashWithdrawal(200, 50449921);
